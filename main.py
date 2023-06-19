8bc7184d69a4cbde5181bd310fc46068e7db61fb
uttamshaw27 <122297633+uttamshaw27@users.noreply.github.com>
2023-06-19 22:05:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("PONG")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.05)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    elif ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+    elif ball.distance(l_paddle) < 50 and ball.xcor() > -350:
+        ball.bounce_x()
+    elif ball.xcor() > 390 or ball.xcor() < -390:
+        game_is_on = False
+
+
+screen.exitonclick()
